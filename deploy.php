aa22aa4ab693cef53032e541287e83e27f4769ce
root <root@68811.local>
2017-03-23 19:15:20 +0300

    Fix deploy

--- deploy.php
+++ deploy.php
@@ -5,18 +5,25 @@
         } elseif (!extension_loaded('hash')) {
                 echo "Missing 'hash' extension to check the secret code validity.";
         }
+
         list($algo, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE'], 2) + array('', '');
         if (!in_array($algo, hash_algos(), TRUE)) {
                 echo "Hash algorithm '$algo' is not supported.";
         }
+
         $rawPost = file_get_contents('php://input');
         if ($hash == hash_hmac($algo, $rawPost, $hookSecret)) {
                 echo 'Hook secret does not match.';
-        $commands = array(
-                'git pull'
-        );
-        foreach($commands AS $command){
-                shell_exec($command);
-        }
-}
+	}
+	else {
+
+		echo 'Secret match';
+        	$commands = array(
+                	'git pull'
+        	);
+
+        	foreach($commands AS $command){
+                	shell_exec($command);
+        	}
+	}
 ?>
